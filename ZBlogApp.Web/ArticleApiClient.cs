8e84c831c26d358f14be350ab7ef7f82c2bd6d46
matt0917 <13935062+matt0917@users.noreply.github.com>
2025-03-19 12:39:13 -0700

    **NOTE**: WebClient Article.razor now use QuickGrid. Not working though. ** Pick the previous submission to use Card style instead. Deleted convert_json_to_docx.py

--- ZBlogApp.Web/ArticleApiClient.cs
+++ ZBlogApp.Web/ArticleApiClient.cs
@@ -1,60 +1,32 @@
 using ZBlogApp.Library;
+using ZBlogApp.Web.Components.Pages;
 
 namespace ZBlogApp.Web;
 
 public class ArticleApiClient(HttpClient httpClient)
 {
-    public async Task<Article[]> GetArticleAsync(int maxItems = 20, CancellationToken cancellationToken = default)
+    public async Task<IQueryable<Article>> GetArticleAsync(CancellationToken cancellationToken = default)
     {
-        List<Article>? articles = null;
 
-        await foreach (var article in httpClient.GetFromJsonAsAsyncEnumerable<Article>("/api/articles", cancellationToken))
-        {
-            if (articles?.Count >= maxItems)
-            {
-                break;
-            }
-            if (article is not null)
-            {
-                articles ??= [];
-                articles.Add(article);
-            }
-        }
-
-        return articles?.ToArray() ?? [];
+        var articles = await httpClient.GetFromJsonAsync<Article[]>("/api/articles", cancellationToken);
+        return articles?.AsQueryable()!;
     }
 
 
-    public async Task<Article[]> GetArticlesInRangeAsync(DateTime start, DateTime end, int maxItems = 20, CancellationToken cancellationToken = default)
-    {
-        List<Article>? articles = null;
-
-        string url = $"/api/articles/range/{start:O}/{end:O}"; // ISO 8601 format
-
-        await foreach (var article in httpClient.GetFromJsonAsAsyncEnumerable<Article>(url, cancellationToken))
-        {
-            if (articles?.Count >= maxItems)
-            {
-                break;
-            }
-            if (article is not null)
-            {
-                articles ??= [];
-                articles.Add(article);
-            }
-        }
-
-        return articles?.ToArray() ?? [];
-    }
-
-    public async Task<Article> GetArticleAsync(string id, CancellationToken cancellationToken = default)
+    public async Task<Article> GetArticleWithIdAsync(string id, CancellationToken cancellationToken = default)
     {
         var article = await httpClient.GetFromJsonAsync<Article>($"/api/articles/{id}", cancellationToken);
-        if (article is null)
-        {
-            throw new InvalidOperationException("Article not found.");
-        }
-        return article;
+        return article!;
+    }
+
+    public async Task<IQueryable<Article>> GetArticlesInRangeAsync(DateTime start, DateTime end, CancellationToken cancellationToken = default)
+    {
+        string url = $"/api/articles/range/{start:O}/{end:O}"; // ISO 8601 format
+
+        var articles = await httpClient.GetFromJsonAsync<Article[]>(url, cancellationToken);
+
+        return articles?.AsQueryable()!;
+
     }
 
 }
